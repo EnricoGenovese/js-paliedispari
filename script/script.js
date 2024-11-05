a6e99d171a8bb6387b8239bb5eea8fbb642fe5b6
EnricoGenovese <enricogeno@libero.it>
2024-11-05 15:23:45 +0100

    init

---
+++ script/script.js
@@ -0,0 +1,17 @@
+"use strict"
+
+console.clear();
+
+/* Esercizio 1 - Palidroma 
+    1 - Chiedere all’utente di inserire una parola
+    2 - Creare una funzione per capire se la parola inserita è palindroma
+*/
+
+/* Esercizio 2 - Pari e Dispari 
+    1 - L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+    2 - Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+    3 - Sommiamo i due numeri
+    4 - Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+    5 - Dichiariamo chi ha vinto.
+*/
+
